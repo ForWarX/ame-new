9317c16ac856e98da23dc9d67882c0b484ca30e1
chyj4747 <chyj4747@163.com>
2018-02-06 15:28:34 -0500

    修改 前后台订单号不一致问题

--- catalog/model/sale/order.php
+++ catalog/model/sale/order.php
@@ -6,32 +6,34 @@ class ModelSaleOrder extends Model {
 
 		$prefix = 'AME';
 		$date = date("ymd", time());
-        if (empty($query->row) || substr($query->row['invoice_prefix'], 0, 3) != $prefix) {
-            $order_no = $prefix . $date . "0000";
-        } else {
-            $no = $query->row['invoice_prefix'];
-            $no = substr($no, 9);
-            $no = ((int)$no + 1) % 10000;
-            $no = sprintf("%04d", $no);
+    if (empty($query->row) || substr($query->row['invoice_prefix'], 0, 3) != $prefix) {
+       $order_no = $prefix . $date . "0000";
+      } else {
+          $no = $query->row['invoice_prefix'];
+           $no = substr($no, 9);
+           $no = ((int)$no + 1) % 10000;
+          $no = sprintf("%04d", $no);
             $order_no = $prefix . $date . $no;
-        }
+     }
 
 		return $order_no;
 	}
-    public function createOrderNumberForShop() {
-        $query = $this->db->query("SELECT invoice_prefix FROM " . DB_PREFIX . "order ORDER BY order_id DESC LIMIT 1");
+    public function createOrderNumberForShop($order_id) {
+        //$query = $this->db->query("SELECT invoice_prefix FROM " . DB_PREFIX . "order ORDER BY order_id DESC LIMIT 1");
 
-        $prefix = 'AES';
+        $prefix = 'AME';
         $date = date("ymd", time());
-        if (empty($query->row) || substr($query->row['invoice_prefix'], 0, 3) != $prefix) {
-            $order_no = $prefix . $date . "0000";
-        } else {
-            $no = $query->row['invoice_prefix'];
-            $no = substr($no, 9);
-            $no = ((int)$no + 1) % 10000;
-            $no = sprintf("%04d", $no);
+       // if (empty($query->row) || substr($query->row['invoice_prefix'], 0, 3) != $prefix) {
+        //    $order_no = $prefix . $date . "0000";
+       // } else {
+           // $no = $query->row['invoice_prefix'];
+           // $no = substr($no, 9);
+         //   $no = ((int)$no + 1) % 10000;
+          //  $no = sprintf("%04d", $no);
+        $no = $order_id% 10000;
+
             $order_no = $prefix . $date . $no;
-        }
+     //   }
 
         return $order_no;
     }
